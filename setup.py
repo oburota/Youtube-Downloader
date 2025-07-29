74818662b8662c025d44acf871dd07c61514a468
oburota <125478348+oburota@users.noreply.github.com>
2025-07-29 11:50:25 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from cx_Freeze import setup, Executable
+
+build_exe_options = {
+    "packages": ["customtkinter", "tkinter", "yt_dlp"],
+    "includes": ["customtkinter", "tkinter", "yt_dlp"]
+}
+
+setup(
+    name="Youtube Downloader",
+    version = "1.0",
+    options={"build_exe": build_exe_options},
+    executables=[Executable("yt_downloader_3.py", base="Win32GUI")]  # Use Win32GUI for GUI apps
+)
